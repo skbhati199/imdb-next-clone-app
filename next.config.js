5cb2937a1461e97682f4521395065f6115924383
sonu kumar <skbhati199@gmail.com>
2023-08-22 18:13:49 +0530

    add github pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = './'
+let basePath = './'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-      },
+    output: 'export',
     images: {
         domains : ["image.tmdb.org"]
     }
