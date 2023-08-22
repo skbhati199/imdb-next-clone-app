cc973f14cb89f3c479621f2b8446652f484b6cbb
sonu kumar <skbhati199@gmail.com>
2023-08-22 11:34:02 +0530

    header tsx add

---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+import { AiFillHome } from "react-icons/ai"
+import { BsFillInfoCircleFill } from "react-icons/bs"
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <div className='flex flex-row justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6'>
+      <div className='flex flex-row'>
+        <MenuItem title='HOME' address="/" Icon={AiFillHome} />
+        <MenuItem title='ABOUT' address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      <div>
+        <Link href="/" >
+          <h2 className='flex items-center text-2xl'>
+          <span className='font-bold bg-amber-600 py-1 px-2 rounded-lg mr-1'>IMDb</span>
+          <span className='text-xl sm:inline hidden'>Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </div>
+  )
+}
