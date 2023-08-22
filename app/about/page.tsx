a83479669b75a3c379cf1f0960bfa4e2de06084f
sonu kumar <skbhati199@gmail.com>
2023-08-22 11:58:07 +0530

    create a about page

---
+++ app/about/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 space-x-4 text-justify">
+      <h1 className="text-2xl font-medium text-amber-600 mx-4">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+        culpa ullam iste fugiat, nostrum maxime ratione hic eius. Magni
+        molestiae corporis illum natus eos laborum quas. Eum architecto vel
+        deserunt.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, illo
+        iusto natus magnam quam id aliquid vitae aperiam fuga ea accusantium
+        voluptates similique quas deleniti vero magni quae fugit sequi! Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Placeat consequuntur
+        maiores mollitia numquam excepturi? Expedita culpa ipsa, delectus
+        consequatur sequi eaque repellendus modi repellat ab a architecto
+        sapiente.
+      </p>
+      <p>
+        
+        Sequi, consectetur? Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Illum, at! Fuga dolores ad soluta, modi ipsa iure necessitatibus
+        molestiae fugiat atque veniam autem magnam placeat vero illo deserunt
+        harum doloribus.
+      </p>
+    </div>
+  );
+}
